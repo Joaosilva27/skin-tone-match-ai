b8fa6643993205639ed829cc4ffc80b5342e3e97
Joaosilva27 <joaosilva7875@gmail.com>
2025-08-02 14:46:15 +0200

    gemini 2.5-flash final version

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import CustomWebcam from "./components/CustomWebcam";
 import "./App.css";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-preview-05-20" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 interface FoundationData {
   name: string;
